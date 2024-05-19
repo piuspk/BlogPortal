743112d25b4810f2fc065ee018e1246608021be4
Piyush Kumar <kumarchspiyush@gmail.com>
2024-05-20 01:51:55 +0530

    chnages

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,5 +1,5 @@
 
 
 // export const BASE_URL = "http://localhost:8000"
-export const BASE_URL = "https://blogportal-od8e.onrender.com"
+export const BASE_URL = "https://blogportal-fldu.onrender.com"
 
