8bcc2405c7477a27fba5bd92a6fdc9f8add14c35
Piyush Kumar <kumarchspiyush@gmail.com>
2024-05-20 02:16:14 +0530

    dfff

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,5 +1,5 @@
 
 
 // export const BASE_URL = "http://localhost:8000"
-export const BASE_URL = "https://blogportal-fldu.onrender.com"
+export const BASE_URL = "https://blogportal-3e9i.onrender.com"
 
