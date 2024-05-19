919699d5e2336ce4491b2a224fc18a5ccd6922c1
Piyush <99687905+piuspk@users.noreply.github.com>
2024-05-20 03:06:52 +0530

    Update upload.utils.js

--- server/utils/upload.utils.js
+++ server/utils/upload.utils.js
@@ -1,11 +1,12 @@
 const multer = require('multer');
 const { GridFsStorage } = require('multer-gridfs-storage');
-require("dotenv").config();
+require('dotenv').config();
+
 const storage = new GridFsStorage({
     url: process.env.URL,
     options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
-        const match = ["image/png", "image/jpg", "image/jpeg"];
+        const match = ['image/png', 'image/jpg', 'image/jpeg'];
 
         if (match.indexOf(file.mimetype) === -1) {
             return `${Date.now()}-blog-${file.originalname}`;
@@ -13,9 +14,9 @@ const storage = new GridFsStorage({
 
         return {
             bucketName: 'photos',
-            filename: `${Date.now()}-blog-${file.originalname}`
+            filename: `${Date.now()}-blog-${file.originalname}`,
         };
-    }
+    },
 });
 
 const upload = multer({ storage });
