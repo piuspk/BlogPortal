4ba93a74583d6ae9cf9e540fc38d69904afe232c
Piyush Kumar <kumarchspiyush@gmail.com>
2024-05-21 16:19:22 +0530

    ss

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,5 +1,6 @@
 
 
 // export const BASE_URL = "http://localhost:8000"
-export const BASE_URL = "https://blogportal-3e9i.onrender.com"
+// export const BASE_URL = "https://blogportal-3e9i.onrender.com"
+export const BASE_URL = "https://blog-server-cyan.vercel.app"
 
